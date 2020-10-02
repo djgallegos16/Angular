0215fdb6ffac9a4fe49a8db698049e8428207df0
usuario <usuario@ubuntu-18.04-desktop-lts-amd64>
2020-10-02 20:51:41 +0200

    loops

--- pstore/src/app/app.component.ts
+++ pstore/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pstore';
+
+  items = ['Daniela', 'Michael', 'Renzo'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla',
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80002,
+      description: 'bla bla bla bla',
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80003,
+      description: 'bla bla bla bla',
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80004,
+      description: 'bla bla bla bla',
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers1',
+      price: 80005,
+      description: 'bla bla bla bla',
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers2',
+      price: 80006,
+      description: 'bla bla bla bla',
+    }
+  ];
+
+  addItem(): void{
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number): void{
+    this.items.splice(index,1);
+  }
 }
